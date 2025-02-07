49cc7406614ba0f7b4043bfdace387c75583bd62
Dolma <dolmatgurung1@gmail.com>
2025-02-06 23:37:09 -0500

    added transaction class attributes

--- transaction.py
+++ transaction.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+
 class transaction:
     def __init__(self, amount, category, transaction_type, date, notes):
-        pass
+        self.amount = amount
+        self.category = category
+        self.transaction_type = transaction_type
+        self.date = date or datetime.now()
+        self.notes = notes
+
+    def __str__(self):
+        return f"{self.transaction_type.capitalize()} | {self.category} | ${self.amount:.2f}"
