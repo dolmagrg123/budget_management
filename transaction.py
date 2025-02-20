39952a99540660907bf46c14adbe3ad2f1e320a2
Dolma <dolmatgurung1@gmail.com>
2025-02-19 19:28:21 -0500

    Removing transaction type and adding date to income/expense classes

--- transaction.py
+++ transaction.py
@@ -1,22 +1,19 @@
 from datetime import datetime
 
 class Transaction:
-    def __init__(self, amount, category, transaction_type, date, notes):
+    def __init__(self, amount, category, date, notes):
         self.amount = amount
         self.category = category
-        self.transaction_type = transaction_type
         self.date = date or datetime.now()
         self.notes = notes
 
-    def __str__(self):
-        return f"{self.transaction_type.capitalize()} | {self.category} | ${self.amount:.2f}"
 
 class Income(Transaction):
     """Class to handle Income entries"""
-    def __init__(self, category, amount):
-        super().__init__(category, amount)
+    def __init__(self, category, amount, date, notes):
+        super().__init__(category, amount, date, notes)
 
 class Expense(Transaction):
     """Class to handle Expense entries"""
-    def __init__(self, category, amount):
-        super().__init__(category, amount)
+    def __init__(self, category, amount, date, notes):
+        super().__init__(category, amount, date, notes)
