6f1f6cabd66c6a2a9a3a3b4f73c86313443ac6ef
Dolma <dolmatgurung1@gmail.com>
2025-02-19 20:50:31 -0500

    Create menu to display to user

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from user import User
+from models.user import User
 
 def main():
     user_name = input("Enter your name: ")
     user = User(user_name)
 
+
+    while True:
+        print("\nBudget Management Menu:")
+        print("1. Set Budget")
+        print("2. Add Income")
+        print("3. Add Expense")
+        print("4. Show Budget")
+        print("5. Exit")
+
+        choice = input("Select an option: ")
 
 if __name__ == "__main__":
     main()
