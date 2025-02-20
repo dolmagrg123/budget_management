09f214a52da55e72cff75755dd80d9b6395f3fa0
Dolma <dolmatgurung1@gmail.com>
2025-02-19 20:47:54 -0500

    Setting methods to add income and  expenses

--- models/user.py
+++ models/user.py
@@ -6,5 +6,11 @@ class User:
     def __init__(self, name):
         self.name = name
         self.budget = Budget()
-        self.income = Income
-        self.expenses = Expense
+        self.income = []
+        self.expenses = []
+
+    def add_income(self,category,amount):
+        self.income.append(Income(category, amount))
+
+    def add_expense(self, category, amount):
+        self.expenses.append(Expense(category, amount))
